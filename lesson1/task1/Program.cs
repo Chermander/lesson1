e72c276d9469f2bbd026741f21b61688cafccdf5
Danila Chernobaev <danila99chernobaew@mail.ru>
2024-02-16 18:08:38 +0200

    add some variables

--- lesson1/task1/Program.cs
+++ lesson1/task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+// comment
+int a = 5; 
+double d = 2.15;
+char c = 'E';
+bool b = false;
+
+Console.WriteLine(a);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(b);
