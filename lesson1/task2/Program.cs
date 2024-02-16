4f04cf78d31bccc8643b0c269e8eefe759074ac9
Danila Chernobaev <danila99chernobaew@mail.ru>
2024-02-16 18:22:16 +0200

    add task2

---
+++ lesson1/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿int i1 = 155;
+
+int i2 = 46;
+
+int sum = i1 + i2;
+
+Console.WriteLine(sum);
+
+long l1 = 6;
+
+long sum2 = l1 +i1;
+
+Console.WriteLine(sum2);
+
+double d1 = 15;
+double sum3 = d1 + i1;
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = d1 * i1;
+
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
